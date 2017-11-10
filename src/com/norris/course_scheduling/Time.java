c6d115bcf7f923e2ae143a5fee066340eb100073
Jose Morales <josedaniel.morales@calbaptist.edu>
2017-11-10 10:26:39 -0800

    basic commit from jose jalapeno

--- src/com/norris/course_scheduling/Time.java
+++ src/com/norris/course_scheduling/Time.java
@@ -7,7 +7,7 @@ public class Time {
 
     private final double[] startTimes = {8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5,
             12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17};
-    private int delete; //delete this
+    private int delete; //delete this thing
 
     public Time() {
 
