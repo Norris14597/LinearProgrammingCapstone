a75da39e1f5ca085fe76c3e91dedc581296b7fbe
anthonyhenson <adhjr.ah@gmail.com>
2017-11-28 01:25:13 -0800

    I moved stuff around. Fixed the class to room size ratio. So far it will assign up to 2 classes to 1 room TR + MWF on in same room or TR + TR in same room I have not seen MWF + MWF in same room

--- src/com/norris/course_scheduling/CourseSchedulingMain.java
+++ src/com/norris/course_scheduling/CourseSchedulingMain.java
@@ -69,103 +69,35 @@ public class CourseSchedulingMain {
 
         double totalCourseSum = 0.0; //sum of each section within a course
         //each course in course list
-        for (Course c : courseList) { //five times
-            double bestSectionSum = 0.0; //best fit for room to section
-            //each section within a course
+        for (Room r: roomList) {
+            double bestSectionSum = 0.0;
+            boolean timeSavedM = false;
+            boolean timeSavedT = false;
 
-            double sectionSum = 0; //individual sum of each section
-            for (Section s : c.getCourseSections()) { //five * 4 = 20 times
+            for (Course c : courseList) {
+                double Hij = LinearProgramming.happinessRoomVal(r, c);
+                double sectionSum = (r.getSeatingCapacity() / c.getCourseSize()) * Hij;
 
-                double Hij = 0; //happiness of courseType to roomType: 1.5 happy, 1 no preference, 0 not happy
-                //int xij = 0; // 1 if section assigned to room, 0 otherwise
-                Professor p = s.getProfessorAssigned(); //professor teaching the section
-                int lastSavedDayIndex = 0; //saves last starting day that meets all requirements
-                int lastSavedTimeIndex = 0; //saves last starting time that meets all requirements
-                boolean timeSaved = false;
+                for (Section s : c.getCourseSections()) {
+                    Professor p = s.getProfessorAssigned();
+                    String[] daysProfessorAvailable = p.getAvailableDaysNames();
 
-                //MWF schedule for professor 5 professors right now
-                String[] daysProfessorAvailable = p.getAvailableDaysNames();
 
-                if (daysProfessorAvailable[0] == "Monday") {
-                    System.out.println(p.getProfessorName() + " It's a Monday!");
-                    int dayIndex = 0; //start on monday
-                    double sectionSumOld = 1;
+                    //boolean timeSaved = false;
 
-                    // all days available for professor
-                    for (int i = 0; i < p.getAvailableDayTimes().size(); i++) {
-                        DayTimes d = p.getAvailableDayTimes().get(i); //individual day
-                        //all times in each day
-                        for (int timeIndex = 0; timeIndex < d.getDayTimes().size(); timeIndex++) {
+                    if (daysProfessorAvailable[0] == "Monday") {
+                        int dayIndex = 0;
+                        double sectionSumOld = 1.0;
 
-                            // hour available; at least an hour before end of day; 3 days available at the same time for professor
-                            if (timeIndex + 4 <= 36 && c.getCredits() * 4 == 12 &&
-                                    LinearProgramming.isProfessorTimeAvailable(timeIndex, p.getAvailableDayTimes())) {
-
-                                //go through each room then view time if a good fit
-                                for (Room r : roomList) {
-
-                                    boolean isRoomAvailable = LinearProgramming.isRoomTimeAvailable(timeIndex, r.getDayList(), "MWF");
-                                    Hij = LinearProgramming.happinessRoomVal(r, c);
-
-                                    sectionSum = (c.getCourseSize() / r.getSeatingCapacity()) * Hij;
-                                    if (sectionSum > sectionSumOld) {
-                                        System.out.println("course size: " + c.getCourseSize() + " room size: " + r.getSeatingCapacity() + " SECTION SUM: " + sectionSum + " happiness: " + Hij + " r type: " + r.getRoomType() + " c type: " + c.getCourseType());
-                                        sectionSumOld = sectionSum;
-                                    }
-
-                                    if (isRoomAvailable && Hij >= 1 && sectionSum > bestSectionSum) {
-                                        bestSectionSum = sectionSum;
-                                        System.out.println("NEW BEST SUM: "+bestSectionSum);
-                                        //System.out.println(r.getBuilding()+r.getRoomNum()+": "+c.getCourseCode()+": "+Hij);
-
-                                        //unassign if previously assigned any time slots
-                                        //will always be true if better sum previously
-                                        if (timeSaved) {
-                                            LinearProgramming.assignSectionToRoom(s, r, lastSavedDayIndex, lastSavedTimeIndex, "MWF", false);
-                                            System.out.println("Reassigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " MWF (time saved)");
-                                        }
-
-                                        lastSavedDayIndex = dayIndex;
-                                        lastSavedTimeIndex = timeIndex;
-                                        timeSaved = true;
-                                        bestSectionSum = sectionSum;
-                                        LinearProgramming.assignSectionToRoom(s, r, lastSavedDayIndex, lastSavedTimeIndex, "MWF", true);
-                                        System.out.println("Assigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " MWF");
-                                    }
-                                }
-                            }
-
-                            //System.out.println(dayIndex+":"+timeIndex);
-                        }
-                        dayIndex++;
-                    }
-                }
-
-                if (daysProfessorAvailable[0] == "Tuesday") {
-                    System.out.println(p.getProfessorName() + " It's a Tuesday!");
-
-                    double sectionSumOld = 1;
-
-                    for (Room r : roomList) {
-
-                        int dayIndex = 1; //start on tuesday
-                    // all days available for professor
                         for (int i = 0; i < p.getAvailableDayTimes().size(); i++) {
                             DayTimes d = p.getAvailableDayTimes().get(i); //individual day
-
-                        //all times in each day
+                            //all times in each day
                             for (int timeIndex = 0; timeIndex < d.getDayTimes().size(); timeIndex++) {
+                                if (timeIndex + 4 <= 36 && c.getCredits() * 4 == 12 &&
+                                        LinearProgramming.isProfessorTimeAvailable(timeIndex, p.getAvailableDayTimes())) {
 
-                            // hour available; at least an hour before end of day; 2 days available at the same time for professor
-                                if (timeIndex + 6 <= 36 && c.getCredits() * 4 == 12 &&
-                                    LinearProgramming.isProfessorTimeAvailable(timeIndex, p.getAvailableDayTimes())) {
+                                    boolean isRoomAvailable = LinearProgramming.isRoomTimeAvailable(timeIndex, r.getDayList(), "MWF");
 
-                                //go through each room then view time if a good fit
-
-                                    boolean isRoomAvailable = LinearProgramming.isRoomTimeAvailable(timeIndex, r.getDayList(), "TR");
-                                    Hij = LinearProgramming.happinessRoomVal(r, c);
-
-                                    sectionSum = (c.getCourseSize() / r.getSeatingCapacity()) * Hij;
                                     if (sectionSum > sectionSumOld) {
                                         System.out.println("course size: " + c.getCourseSize() + " room size: " + r.getSeatingCapacity() + " SECTION SUM: " + sectionSum + " happiness: " + Hij + " r type: " + r.getRoomType() + " c type: " + c.getCourseType());
                                         sectionSumOld = sectionSum;
@@ -173,43 +105,66 @@ public class CourseSchedulingMain {
 
                                     if (isRoomAvailable && Hij >= 1 && sectionSum > bestSectionSum) {
                                         bestSectionSum = sectionSum;
-                                        System.out.println("NEW BEST SUM: "+bestSectionSum);
+                                        System.out.println("NEW BEST SUM: " + bestSectionSum);
                                         //System.out.println(r.getBuilding()+r.getRoomNum()+": "+c.getCourseCode()+": "+Hij);
 
                                         //unassign if previously assigned any time slots
                                         //will always be true if better sum previously
-                                        if (timeSaved) {
-                                            LinearProgramming.assignSectionToRoom(s, r, lastSavedDayIndex, lastSavedTimeIndex, "TR", false);
-                                            System.out.println("Reassigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " TR (time saved)");
+                                        if (timeSavedM) {
+                                            timeIndex++;
+                                            LinearProgramming.assignSectionToRoom(s, r, dayIndex, timeIndex, "MWF", false);
+                                            System.out.println("\nAssigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " MWF\n");
                                         }
-
-                                        lastSavedDayIndex = dayIndex;
-                                        lastSavedTimeIndex = timeIndex;
-                                        timeSaved = true;
+                                        timeSavedM = true;
                                         bestSectionSum = sectionSum;
-                                        LinearProgramming.assignSectionToRoom(s, r, lastSavedDayIndex, lastSavedTimeIndex, "TR", true);
-                                        System.out.println("Assigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " TR");
+                                        LinearProgramming.assignSectionToRoom(s, r, dayIndex, timeIndex, "MWF", true);
+                                        System.out.println("\nAssigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " MWF\n");
                                     }
                                 }
                             }
-
-                            //System.out.println(dayIndex+":"+timeIndex);
+                            dayIndex++;
                         }
-                        dayIndex++;
+                    }
+
+                    if (daysProfessorAvailable[0] == "Tuesday") {
+                        int dayIndex = 1;
+                        double sectionSumOld = 1.0;
+
+                        for (int i = 0; i < p.getAvailableDayTimes().size(); i++) {
+                            DayTimes d = p.getAvailableDayTimes().get(i); //individual day
+                            //all times in each day
+                            for (int timeIndex = 0; timeIndex < d.getDayTimes().size(); timeIndex++) {
+                                if (timeIndex + 6 <= 36 && c.getCredits() * 4 == 12 &&
+                                        LinearProgramming.isProfessorTimeAvailable(timeIndex, p.getAvailableDayTimes())) {
+                                    boolean isRoomAvailable = LinearProgramming.isRoomTimeAvailable(timeIndex, r.getDayList(), "TR");
+                                    if (sectionSum > sectionSumOld) {
+                                        System.out.println("course size: " + c.getCourseSize() + " room size: " + r.getSeatingCapacity() + " SECTION SUM: " + sectionSum + " happiness: " + Hij + " r type: " + r.getRoomType() + " c type: " + c.getCourseType());
+                                        sectionSumOld = sectionSum;
+                                    }
+                                    if (isRoomAvailable && Hij >= 1 && sectionSum > bestSectionSum) {
+                                        bestSectionSum = sectionSum;
+                                        System.out.println("NEW BEST SUM: " + bestSectionSum);
+                                        //System.out.println(r.getBuilding()+r.getRoomNum()+": "+c.getCourseCode()+": "+Hij);
+
+                                        //unassign if previously assigned any time slots
+                                        //will always be true if better sum previously
+                                        if (timeSavedT) {
+                                            timeIndex++;
+                                            LinearProgramming.assignSectionToRoom(s, r, dayIndex, timeIndex, "TR", false);
+                                            System.out.println("\nAssigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " TR\n");
+                                        }
+                                        timeSavedT = true;
+                                        bestSectionSum = sectionSum;
+                                        LinearProgramming.assignSectionToRoom(s, r, dayIndex, timeIndex, "TR", true);
+                                        System.out.println("\nAssigning " + c.getCourseCode() + " taught by " + p + " to classroom " + r.getRoomNum() + " TR\n");
+                                    }
+                                }
+                            }
+                            dayIndex++;
+                        }
                     }
                 }
-
-                //TR schedule for professor
-                //else if (p.getAvailableDayTimes().get(0).getDay() == "Tuesday") {
-
-               // }
-                totalCourseSum += bestSectionSum;
-                //System.out.println("Total course sum " + totalCourseSum);
             }
-
-            totalSum += totalCourseSum;
-            //System.out.println("Total sum " + totalSum);
-
         }
 //        professorList.forEach(System.out::println);
 //        System.out.println();
@@ -230,11 +185,11 @@ public class CourseSchedulingMain {
         //EGR101
         List<Section> sectionList101 = new ArrayList<Section>();
         sectionList101.add(new Section(sectionIds[0], courseName[0]+String.valueOf(sectionIds[0]), professorList.get(0)));
-//        sectionList101.add(new Section(sectionIds[1], courseName[0]+String.valueOf(sectionIds[1]), professorList.get(1)));
-//        sectionList101.add(new Section(sectionIds[2], courseName[0]+String.valueOf(sectionIds[2]), professorList.get(2)));
-//        sectionList101.add(new Section(sectionIds[3], courseName[0]+String.valueOf(sectionIds[3]), professorList.get(3)));
+        sectionList101.add(new Section(sectionIds[1], courseName[0]+String.valueOf(sectionIds[1]), professorList.get(1)));
+        sectionList101.add(new Section(sectionIds[2], courseName[0]+String.valueOf(sectionIds[2]), professorList.get(2)));
+        sectionList101.add(new Section(sectionIds[3], courseName[0]+String.valueOf(sectionIds[3]), professorList.get(3)));
         courseList.add(new Course(courseName[0], 30, 3, courseTypes[0], sectionList101));
-/*
+
         //EGR102
         List<Section> sectionList102 = new ArrayList<Section>();
         sectionList102.add(new Section(sectionIds[0], courseName[1]+String.valueOf(sectionIds[0]), professorList.get(4)));
@@ -266,7 +221,7 @@ public class CourseSchedulingMain {
         sectionList105.add(new Section(sectionIds[2], courseName[4]+String.valueOf(sectionIds[2]), professorList.get(2)));
         sectionList105.add(new Section(sectionIds[3], courseName[4]+String.valueOf(sectionIds[3]), professorList.get(3)));
         courseList.add(new Course(courseName[4], 40, 3, courseTypes[2], sectionList105));
-*/
+
         return courseList;
     }
 
@@ -280,33 +235,30 @@ public class CourseSchedulingMain {
 
         List<Room> roomList = new ArrayList<Room>();
 
-        Room r = new Room("215E", "EGR", 30, "computer");
-        roomList.add(r);
+       // Room r = new Room("215E", "EGR", 30, "computer");
+       // roomList.add(r);
 
-//        int roomSize = 10;
-//        for (int i = 0; i < 5; i++) {
-//            StringBuilder roomNumBuilder = new StringBuilder();
-//            Random rnd = new Random();
-//
-//            //building
-//            int index = (int) (rnd.nextFloat() * buildings.length());
-//            String building = String.valueOf(buildings.charAt(index));
-//            //room number
-//            for (int j = 0; j < 3; j++) {
-//                index = (int) (rnd.nextFloat() * roomNums.length());
-//                roomNumBuilder.append(roomNums.charAt(index));    //set roomNum = to this if u want room num to be 3 digits long
-//            }
-//            String roomNum = roomNumBuilder.toString();
-//            //room type
-//            String roomType = roomTypes[ThreadLocalRandom.current().nextInt(0, 3 + 1)];
-//
-//            //course size
-//            roomSize += 10;
-//            roomList.add(new Room(roomNum, building, roomSize, roomType));
-//        }
+        int roomSize = 10;
+        for (int i = 0; i < 5; i++) {
+            StringBuilder roomNumBuilder = new StringBuilder();
+            Random rnd = new Random();
 
+            //building
+            int index = (int) (rnd.nextFloat() * buildings.length());
+            String building = String.valueOf(buildings.charAt(index));
+            //room number
+            for (int j = 0; j < 3; j++) {
+                index = (int) (rnd.nextFloat() * roomNums.length());
+                roomNumBuilder.append(roomNums.charAt(index));    //set roomNum = to this if u want room num to be 3 digits long
+            }
+            String roomNum = roomNumBuilder.toString();
+            //room type
+            String roomType = roomTypes[ThreadLocalRandom.current().nextInt(0, 3 + 1)];
 
-
+            //course size
+            roomSize += 10;
+            roomList.add(new Room(roomNum, building, roomSize, roomType));
+        }
         return roomList;
     }
 
